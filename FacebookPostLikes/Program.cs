fc22a5705486e792c7fd366f77c2d3c600f2d7a5
Gorgulu Codes <gorgulucodes@gmail.com>
2019-10-13 12:07:19 +0300

    hi adding new files

---
+++ FacebookPostLikes/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacebookPostLikes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a name: ");
+            List<string> name = new List<string>();
+            int i = 0;
+            do
+            {
+                name[i] = Console.ReadLine();
+
+            } while (name[i] != null);
+        }
+    }
+}
